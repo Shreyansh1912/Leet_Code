8ed382c4715c088123ef3a3fd0a668b5d2dbeb71
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-05-23 19:10:04 +0530

    Symmetric Tree

---
+++ src/Tree/TreeNode17.java
@@ -0,0 +1,25 @@
+public class TreeNode17 {
+       int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isSameTree(root.left, root.right);
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        
+        if(p == null || q == null || p.val != q.val) return false;
+       
+        return isSameTree(p.right, q.left) && isSameTree(p.left, q.right) ;
+    }
+}
