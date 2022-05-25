42b2164baf54096e6b653c07c7c862adb3d76037
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-05-25 17:05:38 +0530

    Binary Tree Level Order Traversal II(Reverse Order)

---
+++ src/Tree/TreeNode26.java
@@ -0,0 +1,52 @@
+public class TreeNode26 {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> answer = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        if(root == null) 
+            return answer;
+        
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            
+            int currentSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            
+            while(currentSize > 0)
+            {
+            TreeNode currentNode = queue.remove();
+            currentLevel.add(currentNode.val);
+            
+            
+             if(currentNode.left != null){
+                queue.add(currentNode.left);
+            }
+            
+            if(currentNode.right != null){
+                queue.add(currentNode.right);
+            }
+            currentSize--;
+            
+          }
+        answer.add(currentLevel);
+        }
+        Collections.reverse(answer);
+        return answer;
+        
+        
+    }
+}
